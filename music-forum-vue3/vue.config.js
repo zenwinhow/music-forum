4364f4dee229e28cab5a949d7f421485832b0dd8
ZenWinHow <zenwinhow@outlook.com>
2026-03-09 19:18:16 +0800

    更新后台管理界面中文显示」} PMID? there is stray

--- music-forum-vue3/vue.config.js
+++ music-forum-vue3/vue.config.js
@@ -1,6 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      entry: 'src/main.js',
+      title: 'music-forum音乐论坛'
+    }
+  },
   devServer: {
     proxy: {
       '/api': {
